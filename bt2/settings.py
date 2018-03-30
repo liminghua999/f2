d98f7641a9cce2bc9efeff619bc6f11b3d04ad99
liminghua999 <975412826@qq.com>
2018-03-30 22:19:35 +0800

    change mysql to sqlite3

--- bt2/settings.py
+++ bt2/settings.py
@@ -77,12 +77,14 @@ WSGI_APPLICATION = 'bt2.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': "bt",
-        "HOST":"192.168.0.72",
-        "PORT":"3306",
-        "USER":"lmh",
-        "PASSWORD":"lmh123456",
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        # 'ENGINE': 'django.db.backends.mysql',
+        # 'NAME': "bt",
+        # "HOST":"192.168.0.72",
+        # "PORT":"3306",
+        # "USER":"lmh",
+        # "PASSWORD":"lmh123456",
     }
 }
 

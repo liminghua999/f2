15f38390dcbdb5adcd4a99e410e75ce934093c97
liminghua999 <975412826@qq.com>
2018-03-30 23:15:33 +0800

    增加js日期插件

--- static/js/hostlist.js
+++ static/js/hostlist.js
@@ -3,6 +3,6 @@ $(function () {
     $('#m1-1').find('button').addClass('active');
     $('#m1-1').find('button span').removeClass('glyphicon-chevron-left');
     $('#m1-1').find('button span').addClass('glyphicon-chevron-down');
-    $('#m1-1').find('div').children('a:eq(0)').addClass('active')
-
+    $('#m1-1').find('div').children('a:eq(0)').addClass('active');
+    laydate.render({ elem: '#ctime',type: 'datetime' });
 })
